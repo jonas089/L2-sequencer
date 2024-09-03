bb1db9da9c84caec2e8ad1080030116ead3419db
jonas pauli <jonaspauli089@gmail.com>
2024-09-03 23:27:25 +0200

    automated end to end test for merkle proofs

--- tests/integration.rs
+++ tests/integration.rs
@@ -6,43 +6,72 @@ mod tests {
     };
     use pord_sequencer::types::Transaction;
     use prover::generate_random_number;
-    use reqwest::Client;
-    use std::env;
+    use reqwest::{Client, Response};
+    use std::{env, time::Duration};
+    use tokio::time::sleep;
     use {
         pord_sequencer::config::network::PEERS, pord_sequencer::gossipper::Gossipper,
         pord_sequencer::types::ConsensusCommitment,
     };
 
+    async fn submit_transaction(client: Client, transaction_json: String) -> Response {
+        client
+            .post("http://127.0.0.1:8080/schedule")
+            .header("Content-Type", "application/json")
+            .body(transaction_json.clone())
+            .send()
+            .await
+            .unwrap()
+    }
+
+    async fn request_merkle_proof(client: Client, transaction_key_json: String) -> Response {
+        client
+            .post("http://127.0.0.1:8080/merkle_proof")
+            .header("Content-Type", "application/json")
+            .body(transaction_key_json)
+            .send()
+            .await
+            .unwrap()
+    }
+
+    async fn get_state_root_hash(client: Client) -> Response {
+        client
+            .get("http://127.0.0.1:8080/get/state_root_hash")
+            .send()
+            .await
+            .unwrap()
+    }
+
     #[tokio::test]
-    async fn test_schedule_transaction() {
+    async fn await_transaction_and_verify_merkle_proof() {
         let client = Client::new();
         let transaction: Transaction = Transaction {
             data: vec![1, 2, 3, 4, 5],
             timestamp: 0,
         };
         let transaction_json: String = serde_json::to_string(&transaction).unwrap();
-        // note that currently a transaction may only be submitted to one node
-        // mishandling this can cause the network to crash
-        let response = client
-            .post("http://127.0.0.1:8080/schedule")
-            .header("Content-Type", "application/json")
-            .body(transaction_json.clone())
-            .send()
-            .await
-            .unwrap();
+        let transaction_response = submit_transaction(client.clone(), transaction_json).await;
         assert_eq!(
-            response.text().await.unwrap(),
+            transaction_response.text().await.unwrap(),
             "[Ok] Transaction is being sequenced: Transaction { data: [1, 2, 3, 4, 5], timestamp: 0 }"
         );
-    }
-
-    #[tokio::test]
-    async fn test_request_merkle_proof() {
-        let client = Client::new();
-        let transaction: Transaction = Transaction {
-            data: vec![1, 2, 3, 4, 5],
-            timestamp: 0,
-        };
+        let mut node_trie_root: Option<Root> = None;
+        for _5_seconds in 0..100 {
+            let trie_root_json = get_state_root_hash(client.clone())
+                .await
+                .text()
+                .await
+                .unwrap();
+            let trie_root: Root = serde_json::from_str(&trie_root_json).unwrap();
+            match trie_root.hash.clone() {
+                Some(_) => {
+                    node_trie_root = Some(trie_root);
+                    break;
+                }
+                None => {}
+            };
+            sleep(Duration::from_secs(5)).await;
+        }
         let mut leaf = Leaf::new(Vec::new(), Some(transaction.data.clone()));
         leaf.hash();
         leaf.key = leaf
@@ -54,26 +83,34 @@ mod tests {
             .collect();
         leaf.hash();
         let transaction_key_json = serde_json::to_string(&leaf.key).unwrap();
-        let merkle_proof_response = client
-            .post("http://127.0.0.1:8080/merkle_proof")
-            .header("Content-Type", "application/json")
-            .body(transaction_key_json)
-            .send()
-            .await
-            .unwrap();
+        let merkle_proof_response =
+            request_merkle_proof(client.clone(), transaction_key_json).await;
         let merkle_proof_json = merkle_proof_response.text().await.unwrap();
         let merkle_proof: MerkleProof = serde_json::from_str(&merkle_proof_json).unwrap();
-        let state_root_hash_response = client
-            .get("http://127.0.0.1:8080/get/state_root_hash")
-            .send()
-            .await
-            .unwrap();
-        let state_root_hash: Root =
-            serde_json::from_str(&state_root_hash_response.text().await.unwrap()).unwrap();
-        let mut inner_proof = merkle_proof.nodes;
-        inner_proof.reverse();
-        println!("Inner Proof: {:?}", &inner_proof);
-        verify_merkle_proof(inner_proof, state_root_hash.hash.unwrap());
+        verify_merkle_proof(
+            merkle_proof.nodes,
+            node_trie_root
+                .expect("[Error] No Trie Root present!")
+                .hash
+                .unwrap(),
+        );
+    }
+
+    #[tokio::test]
+    async fn test_schedule_transaction() {
+        let client = Client::new();
+        let transaction: Transaction = Transaction {
+            data: vec![1, 2, 3, 4, 5],
+            timestamp: 0,
+        };
+        let transaction_json: String = serde_json::to_string(&transaction).unwrap();
+        // note that currently a transaction may only be submitted to one node
+        // mishandling this can cause the network to crash
+        let transaction_response = submit_transaction(client, transaction_json).await;
+        assert_eq!(
+            transaction_response.text().await.unwrap(),
+            "[Ok] Transaction is being sequenced: Transaction { data: [1, 2, 3, 4, 5], timestamp: 0 }"
+        );
     }
 
     #[tokio::test]
